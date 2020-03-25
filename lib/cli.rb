10c7123edbdf2854b67989f55d009c1bd4804d9d
Dawit Gizaw <gizawdawit123.com>
2020-03-25 17:32:14 -0400

    added descriptions to all methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -36,19 +36,19 @@ class CommandLineInterface
         show_logo
     end
 
-    # NEW ******************
+    # Exits program
     def exit_program
         puts "Goodbye!!!"
     end
 
     
     def display_menu
-        menu = ["Search song","Artists", "Albums", "Genre", "Songs","To Exit" ]
+        menu = ["Search song","Artists", "Albums", "Genre", "Songs","To Exit", "Search album" ]
         user_choice = prompt("Select a menu item please: ", menu)
         return user_choice
-        # puts "6. to make your own playlist"
     end
 
+    #Searches for users input of a song
     def search_song
       puts "Enter the song name:"
       song = get_user_input.titleize
@@ -59,36 +59,52 @@ class CommandLineInterface
           song_instance = Song.find_by(name: song)
         end
         return song_instance.name
-           
     end
 
+
+    # Searches for users input of an album
+    def search_album
+      puts "Enter the album name:"
+      album = get_user_input.titleize
+      album_instance = Album.find_by(title: album)
+        while !album_instance do
+          puts "Invalid, try again:"
+          album = get_user_input.titleize
+          album_instance = Album.find_by(title: album)
+        end
+        var =  album_instance.title
+    end
+
+    #plays a song
     def play(song)# needs validation for when song does't exist in library
         puts "Playing #{song}"
     end
 
+    # Returns a list of artists
     def view_all_artists
         list_of_artists = []
         info_array = Artist.all.each{|artist|  list_of_artists << artist.name}.sort        
        prompt("Artists: ", list_of_artists)
     end
  
-
+    # Returns the songs of an Artist
     def view_artist_songs(artist)
         artist_name = artist
         artist_id = Artist.find_by(name: artist_name).id
         songs = Song.where(artist_id: artist_id).map{|song| song.name}
-        count = 1
         puts "**************************"
         puts "**************************"
         prompt("#{artist_name}'s songs", songs)
     end
 
+    # Returns a list of Albums
     def view_all_albums
       list_of_albums = []
         info_array = Album.all.each{|album|  list_of_albums << album.title}.sort        
        prompt("Albums: ", list_of_albums)
     end
 
+    # Returns a list of songs by Album
     def view_album_songs(album)
       album_title = album
       album_id = Album.find_by(title: album_title).id
@@ -98,24 +114,24 @@ class CommandLineInterface
       prompt("#{album_title}'s songs", songs)
     end
 
-    def search_for_album(album_name)
-        prompt = TTY::Prompt.new
-
-        puts "Enter the album's title: "
-        album_title = get_user_input.titleize
-        album_id = Album.all.find_by(title: album_title).id
-        songs = Song.where(album_id: album_id)
+    # Searches with an album's name and returns the songs for the album
+    # def search_for_album(album_name)
+    #     puts "Enter the album's title: "
+    #     album_title = get_user_input.titleize
+    #     album_id = Album.all.find_by(title: album_title).id
+    #     songs = Song.where(album_id: album_id)
        
-        puts "**************************"
-        puts "#{album_title}"
-        puts "**************************"
-        count = 1
-        songs.each do |song|
-            ap "#{count}. #{song.name}" 
-            count += 1
-        end
-    end
+    #     puts "**************************"
+    #     puts "#{album_title}"
+    #     puts "**************************"
+    #     count = 1
+    #     songs.each do |song|
+    #         ap "#{count}. #{song.name}" 
+    #         count += 1
+    #     end
+    # end
 
+    # Returns a list of artists by the selected genre
     def artist_by_genre
         all_genres = Artist.all.map{|artist| artist.genre}.uniq.sort
         genre_name = prompt("Choose a genre: ", all_genres)
@@ -126,58 +142,15 @@ class CommandLineInterface
         prompt("Choose an Artist from #{genre_name}", artists)
     end
 
-    # def make_your_own_playlist
-    #     puts "Enter your album's name: "
-    #     album_name = gets.chomp
-    #     Album.create(name: album_name)
-    #     puts "Which songs do you want to add to your playlist?"
-    #     display_all_songs
-
-    # end 
-
+    # Returns all songs in database
     def display_all_songs
       all_songs = Song.all.map{|song| song.name}.sort
       song_name = prompt("Choose a song: ", all_songs)
     end
 
     
-
-  
-     
-    # # NEW ******************
-    # def help
-    #     puts "I accept the following commands:"
-    #     puts "- Menu"
-    #     puts "- Help"
-    #     puts "- Search"
-    #     puts "- Exit"
-    # end
-
-    # def run
-    #     menu_array = ["To play a song by name","To view all artists", "To view all songs by artist", "To view all songs by album", "To view all artists by genre", "To see all songs" ]
-
-    #     choice = display_menu
-    #     if choice == menu_array[0]
-    #         play
-    #     elsif choice == menu_array[1]
-    #         view_all_artists  
-    #     elsif choice == menu_array[2]
-    #         view_artists_songs
-    #     elsif choice == menu_array[3]
-    #         view_album_songs
-    #     elsif choice == menu_array[4]
-    #         artist_by_genre
-    #     # elsif choice == 6
-    #     #     make_your_own_playlist
-    #     elsif choice == menu_array[5]
-    #         display_all_songs
-    #     end
-    
-    #     # prompt = TTY::Prompt.new
-    #     # prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
-    # end
     def run
-        menu_array = ["Search song","Artists", "Albums", "Genre", "Songs","To Exit" ]
+        menu_array = ["Search song","Artists", "Albums", "Genre", "Songs","To Exit", "Search album"]
           
             input = ""
             while input
@@ -196,6 +169,8 @@ class CommandLineInterface
               when menu_array[5] #EXIT
                 exit_program
                 break
+              when menu_array[6]
+                play(view_album_songs(search_album))
               else
                 display_menu
               end
